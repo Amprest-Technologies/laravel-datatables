244710295cba64580508df5887b0bfc8fb417336
Alvin Kaburu <geekaburu@amprest.co.ke>
2020-01-29 10:37:13 +0300

    Cleaned up code references

--- src/Datatables.php
+++ src/Datatables.php
@@ -16,63 +16,12 @@ class Datatables
     public static function payload($tableID)
     {        
         // 	Define defaults, and fetch configurations
-        $configuration = Configuration::where('identifier', $tableID)
-            ->first();
+        $configuration = Configuration::identifier($tableID)->first();
         
         //  Check if a configuration was drawn
         if($configuration) return $configuration->payload;
 
         //  Throw a 404 error otherwise
         abort('404', "The table's identifier, $tableID cannot be found or has been deactivated.");
-
-        return array_merge([
-            'id' => 'users-table'
-        ], config('datatables'));
-
-        // 	Define defaults, and fetch configurations
-        $identifier = $identifier ?: $tableID;
-        $data = Self::data($identifier);
-        $defaults = Self::defaults($tableID);
-
-        // //  Merge the defaults with the determined payload, remove any extra keys
-        // return dd(json_encode((
-        //     collect(array_merge($defaults, $data))->forget('custom')
-        // )->toArray()));
-
-        //  Merge the defaults with the determined payload, remove any extra keys
-        return (
-            collect(array_merge($defaults, $data))->forget('custom')
-        )->toArray();
     }   
-    
-    /**
-     * Generate custom datatables data.
-     *
-     * @author Alvin Gichira Kaburu <geekaburu@amprest.co.ke>
-     * @param String $identifier
-     * @return Array
-     */
-    public static function data($identifier)
-    {
-        // Get the customized data as defined by the identifier
-        return config('datatables.data.'.$identifier);
-    }
-
-    /**
-     * Generate the default configurations.
-     *
-     * @author Alvin Gichira Kaburu <geekaburu@amprest.co.ke>
-     * @param String $tableID
-     * @return Array
-     */
-    public static function defaults($tableID)
-    {
-        //  Get the general defaults from the config and append a 
-        //  key named exports and id with the defined export values.
-        return array_merge([
-            'id' => $tableID,
-        ], config('datatables.defaults'), [
-            'exports' => config('datatables.exports'),
-        ]);
-    }
 }
