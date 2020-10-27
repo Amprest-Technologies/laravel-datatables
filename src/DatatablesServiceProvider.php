c2647e05e4deaa07977616222c1bd8f731b09145
Paul Kinyanjui <kinyanjuipaul34@gmail.com>
2020-10-27 19:19:37 +0300

    Refactored for Composer 2

--- src/DatatablesServiceProvider.php
+++ src/DatatablesServiceProvider.php
@@ -18,45 +18,57 @@ class DatatablesServiceProvider extends ServiceProvider
     }
 
     /**
+     * Load helper functions.
+     *
+     * @return void
+     */
+    protected function loadHelpers()
+    {
+        foreach (glob(__DIR__ . DIRECTORY_SEPARATOR . 'Utils' . DIRECTORY_SEPARATOR . '*.php') as $helper) {
+            require_once $helper;
+        }
+    }
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
-    {    
-        //  Get the root path  
-        $root =  __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..';
-         
+    {
+        //  Get the root path
+        $root = __DIR__ . DIRECTORY_SEPARATOR . '..';
+
         //  Register default config values
-        $this->mergeConfigFrom($root.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'package.php', 'package');
-        $this->mergeConfigFrom($root.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'datatables.php', 'datatables');
+        $this->mergeConfigFrom($root . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'package.php', 'package');
+        $this->mergeConfigFrom($root . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'datatables.php', 'datatables');
 
         //  Register dependent service providers.
         $this->app->register('Amprest\LaravelDatatables\Providers\BladeServiceProvider');
         $this->app->register('Amprest\LaravelDatatables\Providers\FacadeServiceProvider');
-        
+
         $routePrefix = config('datatables.route.prefix');
         $this->app['router']
             ->name('datatables.')
-            ->prefix( ( $routePrefix ? $routePrefix.'/' : '' ).'datatables')
+            ->prefix(($routePrefix ? $routePrefix . '/' : '') . 'datatables')
             ->namespace('Amprest\\LaravelDatatables\\Controllers')
-            ->middleware([ 'web' ])
+            ->middleware(['web'])
             ->group(function () {
-                $this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'routes/web.php');
+                $this->loadRoutesFrom(__DIR__ . DIRECTORY_SEPARATOR .  '..' . DIRECTORY_SEPARATOR . 'routes/web.php');
             });
 
         //  Load other package file dependancies
-        $this->loadViewsFrom($root.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views', config('package.name'));
-        $this->loadMigrationsFrom($root.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations');
+        $this->loadViewsFrom($root . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views', config('package.name'));
+        $this->loadMigrationsFrom($root . DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR . 'migrations');
 
         //  Allow the config files to be published.
-        $this->publishes( [
-            $root.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'datatables.php' => config_path('datatables.php') 
+        $this->publishes([
+            $root . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'datatables.php' => config_path('datatables.php')
         ], 'datatables-config');
 
         //  Allow public assets to be published
         $this->publishes([
-            $root.DIRECTORY_SEPARATOR.'public' => public_path('vendor'.DIRECTORY_SEPARATOR.''.config('package.name')),
+            $root . DIRECTORY_SEPARATOR . 'public' => public_path('vendor' . DIRECTORY_SEPARATOR . '' . config('package.name')),
         ], 'datatables-assets');
 
         //  Register custom package commands
@@ -64,18 +76,6 @@ class DatatablesServiceProvider extends ServiceProvider
             $this->commands([
                 Console\Commands\DatatablesInstall::class,
             ]);
-        }
-    }
-
-    /**
-     * Load helper functions.
-     *
-     * @return void
-     */
-    protected function loadHelpers()
-    {
-        foreach (glob(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Utils'.DIRECTORY_SEPARATOR.'*.php') as $helper) {
-            require_once $helper;
         }
     }
 }
