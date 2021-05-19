8bb13b4bb0128eb7500f47b48e73f60f40617635
Alvin Kaburu <geekaburu@amprest.co.ke>
2021-05-19 12:38:54 +0300

    Reconfigured the facade accessor

--- src/Facades/Datatables.php
+++ src/Facades/Datatables.php
@@ -2,29 +2,19 @@
 
 namespace Amprest\LaravelDatatables\Facades;
 
-class Datatables
+use Amprest\LaravelDatatables\Datatables as LaravelDatatable;
+use Illuminate\Support\Facades\Facade;
+
+class Datatables extends Facade
 {
     /**
-     *  This function resolves the facade from the app container
-     *  @author Alvin Gichira Kaburu
-     *  @param string $name
-     *  @return class
+     * Get the registered name of the component.
+     *
+     * @return string
+     * @author Alvin G. Kaburu <geekaburu@amprest.co.ke>
      */
-    protected static function resolveFacade($name)
-    {
-        return app()->make($name);
-    }
-
-    /**
-     *  This function class the method from the parent class
-     *  passing any relevant arguments
-     *  @author Alvin Gichira Kaburu
-     *  @param string $method
-     *  @param array $arguments
-     */
-    public static function __callStatic($method, $arguments)
-    {
-        return ( self::resolveFacade('Datatables') )
-            ->$method(...$arguments);
+    protected static function getFacadeAccessor(): string
+    { 
+        return LaravelDatatable::class; 
     }
 }
